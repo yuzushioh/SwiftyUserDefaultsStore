6b7fc06270caf13948aaafa3d6991e46a57994d1
Ryo <eeee.jmk@gmail.com>
2016-08-27 03:40:47 +0900

    reduce func and keep static func only

--- SwiftyUserDefaultsStore/SwiftyUserDefaultsStore.swift
+++ SwiftyUserDefaultsStore/SwiftyUserDefaultsStore.swift
@@ -28,24 +28,6 @@ public protocol SwiftyUserDefaultsStoreType {
     static func setValue(value: Double?, forKey key: UserDefaultsStoreKey)
     static func setValue(value: String?, forKey key: UserDefaultsStoreKey)
     static func setValue(value: NSDate?, forKey key: UserDefaultsStoreKey)
-    
-    var userDefaults: NSUserDefaults { get }
-    
-    func hasValueForKey(key: UserDefaultsStoreKey) -> Bool
-    
-    func valueForKey(key: UserDefaultsStoreKey) -> Bool?
-    func valueForKey(key: UserDefaultsStoreKey) -> Int?
-    func valueForKey(key: UserDefaultsStoreKey) -> Int64?
-    func valueForKey(key: UserDefaultsStoreKey) -> Double?
-    func valueForKey(key: UserDefaultsStoreKey) -> String?
-    func valueForKey(key: UserDefaultsStoreKey) -> NSDate?
-    
-    func setValue(value: Bool?, forKey key: UserDefaultsStoreKey)
-    func setValue(value: Int?, forKey key: UserDefaultsStoreKey)
-    func setValue(value: Int64?, forKey key: UserDefaultsStoreKey)
-    func setValue(value: Double?, forKey key: UserDefaultsStoreKey)
-    func setValue(value: String?, forKey key: UserDefaultsStoreKey)
-    func setValue(value: NSDate?, forKey key: UserDefaultsStoreKey)
 }
 
 public protocol SwiftyUserDefaultsStoreKeyConvertible {
@@ -111,70 +93,6 @@ extension SwiftyUserDefaultsStoreType {
     }
     
     static func setValue(value: NSDate?, forKey key: UserDefaultsStoreKey) {
-        userDefaults.setObject(value, forKey: key.userDefaultsKeyString)
-        userDefaults.synchronize()
-    }
-}
-
-extension SwiftyUserDefaultsStoreType {
-    var userDefaults: NSUserDefaults {
-        return NSUserDefaults.standardUserDefaults()
-    }
-    
-    func hasValueForKey(key: UserDefaultsStoreKey) -> Bool {
-        return userDefaults.objectForKey(key.userDefaultsKeyString) != nil
-    }
-
-    func valueForKey(key: UserDefaultsStoreKey) -> Bool? {
-        return userDefaults.objectForKey(key.userDefaultsKeyString) as? Bool
-    }
-    
-    func valueForKey(key: UserDefaultsStoreKey) -> Int? {
-        return userDefaults.objectForKey(key.userDefaultsKeyString) as? Int
-    }
-    
-    func valueForKey(key: UserDefaultsStoreKey) -> Int64? {
-        return userDefaults.objectForKey(key.userDefaultsKeyString) as? Int64
-    }
-    
-    func valueForKey(key: UserDefaultsStoreKey) -> Double? {
-        return userDefaults.objectForKey(key.userDefaultsKeyString) as? Double
-    }
-    
-    func valueForKey(key: UserDefaultsStoreKey) -> String? {
-        return userDefaults.objectForKey(key.userDefaultsKeyString) as? String
-    }
-    
-    func valueForKey(key: UserDefaultsStoreKey) -> NSDate? {
-        return userDefaults.objectForKey(key.userDefaultsKeyString) as? NSDate
-    }
-    
-    func setValue(value: Bool?, forKey key: UserDefaultsStoreKey) {
-        userDefaults.setObject(value, forKey: key.userDefaultsKeyString)
-        userDefaults.synchronize()
-    }
-    
-    func setValue(value: Int?, forKey key: UserDefaultsStoreKey) {
-        userDefaults.setObject(value, forKey: key.userDefaultsKeyString)
-        userDefaults.synchronize()
-    }
-    
-    func setValue(value: Int64?, forKey key: UserDefaultsStoreKey) {
-        userDefaults.setObject(value.map(NSNumber.init), forKey: key.userDefaultsKeyString)
-        userDefaults.synchronize()
-    }
-    
-    func setValue(value: Double?, forKey key: UserDefaultsStoreKey) {
-        userDefaults.setObject(value, forKey: key.userDefaultsKeyString)
-        userDefaults.synchronize()
-    }
-    
-    func setValue(value: String?, forKey key: UserDefaultsStoreKey) {
-        userDefaults.setObject(value, forKey: key.userDefaultsKeyString)
-        userDefaults.synchronize()
-    }
-    
-    func setValue(value: NSDate?, forKey key: UserDefaultsStoreKey) {
         userDefaults.setObject(value, forKey: key.userDefaultsKeyString)
         userDefaults.synchronize()
     }
