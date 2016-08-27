fba007dbbc89f767d7dd859f7de8a20fa03eefd9
Ryo <eeee.jmk@gmail.com>
2016-08-27 20:38:30 +0900

    sampleを作成

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -17,3 +17,23 @@ class ViewController: UIViewController {
     }
 }
 
+struct UserDefaultsKeyStore {
+    let username = UserDefaultsStoreKey<String>(key: "username")
+    let userId = UserDefaultsStoreKey<Int64?>(key: "userId")
+    let openAppCount = UserDefaultsStoreKey<Int>(key: "openAppCount")
+    let isLoggedIn = UserDefaultsStoreKey<Bool>(key: "isLoggedIn")
+}
+
+class UserState: SwiftyUserDefaultsStoreType {
+    let store = UserDefaultsKeyStore()
+    
+    var username: String {
+        get {
+            return valueForStoreKey(store.username)
+        }
+        set {
+            setNewValue(newValue, forStoreKey: store.username)
+        }
+    }
+}
+
