1387f463a259b37b72dddda08a448aa5a858bc52
Ryo <eeee.jmk@gmail.com>
2016-08-27 20:38:19 +0900

    StoreTypeの方針を変更

--- Source/SwiftyUserDefaultsStoreType.swift
+++ Source/SwiftyUserDefaultsStoreType.swift
@@ -11,29 +11,29 @@ import Foundation
 public protocol SwiftyUserDefaultsStoreType {
     var userDefaults: NSUserDefaults { get }
     
-    func userDefaultsValueForKey(storeKey: UserDefaultsStoreKey<Bool?>) -> Bool?
-    func userDefaultsValueForKey(storeKey: UserDefaultsStoreKey<Int?>) -> Int?
-    func userDefaultsValueForKey(storeKey: UserDefaultsStoreKey<Int64?>) -> Int64?
-    func userDefaultsValueForKey(storeKey: UserDefaultsStoreKey<Double?>) -> Double?
-    func userDefaultsValueForKey(storeKey: UserDefaultsStoreKey<String?>) -> String?
+    func valueForStoreKey(storeKey: UserDefaultsStoreKey<Bool?>) -> Bool?
+    func valueForStoreKey(storeKey: UserDefaultsStoreKey<Int?>) -> Int?
+    func valueForStoreKey(storeKey: UserDefaultsStoreKey<Int64?>) -> Int64?
+    func valueForStoreKey(storeKey: UserDefaultsStoreKey<Double?>) -> Double?
+    func valueForStoreKey(storeKey: UserDefaultsStoreKey<String?>) -> String?
     
-    func userDefaultsValueForKey(storeKey: UserDefaultsStoreKey<Bool>) -> Bool
-    func userDefaultsValueForKey(storeKey: UserDefaultsStoreKey<Int>) -> Int
-    func userDefaultsValueForKey(storeKey: UserDefaultsStoreKey<Int64>) -> Int64
-    func userDefaultsValueForKey(storeKey: UserDefaultsStoreKey<Double>) -> Double
-    func userDefaultsValueForKey(storeKey: UserDefaultsStoreKey<String>) -> String
+    func valueForStoreKey(storeKey: UserDefaultsStoreKey<Bool>) -> Bool
+    func valueForStoreKey(storeKey: UserDefaultsStoreKey<Int>) -> Int
+    func valueForStoreKey(storeKey: UserDefaultsStoreKey<Int64>) -> Int64
+    func valueForStoreKey(storeKey: UserDefaultsStoreKey<Double>) -> Double
+    func valueForStoreKey(storeKey: UserDefaultsStoreKey<String>) -> String
     
-    func setUserDefaultsValue(value: Bool?, forKey storeKey: UserDefaultsStoreKey<Bool?>)
-    func setUserDefaultsValue(value: Int?, forKey storeKey: UserDefaultsStoreKey<Int?>)
-    func setUserDefaultsValue(value: Int64?, forKey storeKey: UserDefaultsStoreKey<Int64?>)
-    func setUserDefaultsValue(value: Double?, forKey storeKey: UserDefaultsStoreKey<Double?>)
-    func setUserDefaultsValue(value: String?, forKey storeKey: UserDefaultsStoreKey<String?>)
+    func setNewValue(value: Bool?, forStoreKey storeKey: UserDefaultsStoreKey<Bool?>)
+    func setNewValue(value: Int?, forStoreKey storeKey: UserDefaultsStoreKey<Int?>)
+    func setNewValue(value: Int64?, forStoreKey storeKey: UserDefaultsStoreKey<Int64?>)
+    func setNewValue(value: Double?, forStoreKey storeKey: UserDefaultsStoreKey<Double?>)
+    func setNewValue(value: String?, forStoreKey storeKey: UserDefaultsStoreKey<String?>)
     
-    func setUserDefaultsValue(value: Bool, forKey storeKey: UserDefaultsStoreKey<Bool>)
-    func setUserDefaultsValue(value: Int, forKey storeKey: UserDefaultsStoreKey<Int>)
-    func setUserDefaultsValue(value: Int64?, forKey storeKey: UserDefaultsStoreKey<Int64>)
-    func setUserDefaultsValue(value: Double, forKey storeKey: UserDefaultsStoreKey<Double>)
-    func setUserDefaultsValue(value: String, forKey storeKey: UserDefaultsStoreKey<String>)
+    func setNewValue(value: Bool, forStoreKey storeKey: UserDefaultsStoreKey<Bool>)
+    func setNewValue(value: Int, forStoreKey storeKey: UserDefaultsStoreKey<Int>)
+    func setNewValue(value: Int64?, forStoreKey storeKey: UserDefaultsStoreKey<Int64>)
+    func setNewValue(value: Double, forStoreKey storeKey: UserDefaultsStoreKey<Double>)
+    func setNewValue(value: String, forStoreKey storeKey: UserDefaultsStoreKey<String>)
 }
 
 public extension SwiftyUserDefaultsStoreType {
@@ -41,27 +41,27 @@ public extension SwiftyUserDefaultsStoreType {
         return NSUserDefaults.standardUserDefaults()
     }
     
-    func userDefaultsValueForKey(storeKey: UserDefaultsStoreKey<Bool?>) -> Bool? {
+    func valueForStoreKey(storeKey: UserDefaultsStoreKey<Bool?>) -> Bool? {
         return userDefaults.objectForKey(storeKey.key) as? Bool
     }
     
-    func userDefaultsValueForKey(storeKey: UserDefaultsStoreKey<Int?>) -> Int? {
+    func valueForStoreKey(storeKey: UserDefaultsStoreKey<Int?>) -> Int? {
         return userDefaults.objectForKey(storeKey.key) as? Int
     }
     
-    func userDefaultsValueForKey(storeKey: UserDefaultsStoreKey<Int64?>) -> Int64? {
+    func valueForStoreKey(storeKey: UserDefaultsStoreKey<Int64?>) -> Int64? {
         return userDefaults.objectForKey(storeKey.key) as? Int64
     }
     
-    func userDefaultsValueForKey(storeKey: UserDefaultsStoreKey<Double?>) -> Double? {
+    func valueForStoreKey(storeKey: UserDefaultsStoreKey<Double?>) -> Double? {
         return userDefaults.objectForKey(storeKey.key) as? Double
     }
     
-    func userDefaultsValueForKey(storeKey: UserDefaultsStoreKey<String?>) -> String? {
+    func valueForStoreKey(storeKey: UserDefaultsStoreKey<String?>) -> String? {
         return userDefaults.objectForKey(storeKey.key) as? String
     }
     
-    func userDefaultsValueForKey(storeKey: UserDefaultsStoreKey<Bool>) -> Bool {
+    func valueForStoreKey(storeKey: UserDefaultsStoreKey<Bool>) -> Bool {
         guard let value = userDefaults.objectForKey(storeKey.key) as? Bool else {
             return false
         }
@@ -69,7 +69,7 @@ public extension SwiftyUserDefaultsStoreType {
         return value
     }
     
-    func userDefaultsValueForKey(storeKey: UserDefaultsStoreKey<Int>) -> Int {
+    func valueForStoreKey(storeKey: UserDefaultsStoreKey<Int>) -> Int {
         guard let value = userDefaults.objectForKey(storeKey.key) as? Int else {
             return 0
         }
@@ -77,7 +77,7 @@ public extension SwiftyUserDefaultsStoreType {
         return value
     }
     
-    func userDefaultsValueForKey(storeKey: UserDefaultsStoreKey<Int64>) -> Int64 {
+    func valueForStoreKey(storeKey: UserDefaultsStoreKey<Int64>) -> Int64 {
         guard let value = userDefaults.objectForKey(storeKey.key) as? Int64 else {
             return 0
         }
@@ -85,7 +85,7 @@ public extension SwiftyUserDefaultsStoreType {
         return value
     }
     
-    func userDefaultsValueForKey(storeKey: UserDefaultsStoreKey<Double>) -> Double {
+    func valueForStoreKey(storeKey: UserDefaultsStoreKey<Double>) -> Double {
         guard let value = userDefaults.objectForKey(storeKey.key) as? Double else {
             return 0.0
         }
@@ -93,7 +93,7 @@ public extension SwiftyUserDefaultsStoreType {
         return value
     }
     
-    func userDefaultsValueForKey(storeKey: UserDefaultsStoreKey<String>) -> String {
+    func valueForStoreKey(storeKey: UserDefaultsStoreKey<String>) -> String {
         guard let value = userDefaults.objectForKey(storeKey.key) as? String else {
             return ""
         }
@@ -101,52 +101,52 @@ public extension SwiftyUserDefaultsStoreType {
         return value
     }
     
-    func setUserDefaultsValue(value: Bool?, forKey storeKey: UserDefaultsStoreKey<Bool?>) {
+    func setNewValue(value: Bool?, forStoreKey storeKey: UserDefaultsStoreKey<Bool?>) {
         userDefaults.setObject(value, forKey: storeKey.key)
         userDefaults.synchronize()
     }
     
-    func setUserDefaultsValue(value: Int?, forKey storeKey: UserDefaultsStoreKey<Int?>) {
+    func setNewValue(value: Int?, forStoreKey storeKey: UserDefaultsStoreKey<Int?>) {
         userDefaults.setObject(value, forKey: storeKey.key)
         userDefaults.synchronize()
     }
     
-    func setUserDefaultsValue(value: Int64?, forKey storeKey: UserDefaultsStoreKey<Int64?>) {
+    func setNewValue(value: Int64?, forStoreKey storeKey: UserDefaultsStoreKey<Int64?>) {
         userDefaults.setObject(value.map(NSNumber.init), forKey: storeKey.key)
         userDefaults.synchronize()
     }
     
-    func setUserDefaultsValue(value: Double?, forKey storeKey: UserDefaultsStoreKey<Double?>) {
+    func setNewValue(value: Double?, forStoreKey storeKey: UserDefaultsStoreKey<Double?>) {
         userDefaults.setObject(value, forKey: storeKey.key)
         userDefaults.synchronize()
     }
     
-    func setUserDefaultsValue(value: String?, forKey storeKey: UserDefaultsStoreKey<String?>) {
+    func setNewValue(value: String?, forStoreKey storeKey: UserDefaultsStoreKey<String?>) {
         userDefaults.setObject(value, forKey: storeKey.key)
         userDefaults.synchronize()
     }
     
-    func setUserDefaultsValue(value: Bool, forKey storeKey: UserDefaultsStoreKey<Bool>) {
+    func setNewValue(value: Bool, forStoreKey storeKey: UserDefaultsStoreKey<Bool>) {
         userDefaults.setObject(value, forKey: storeKey.key)
         userDefaults.synchronize()
     }
     
-    func setUserDefaultsValue(value: Int, forKey storeKey: UserDefaultsStoreKey<Int>) {
+    func setNewValue(value: Int, forStoreKey storeKey: UserDefaultsStoreKey<Int>) {
         userDefaults.setObject(value, forKey: storeKey.key)
         userDefaults.synchronize()
     }
     
-    func setUserDefaultsValue(value: Int64?, forKey storeKey: UserDefaultsStoreKey<Int64>) {
+    func setNewValue(value: Int64?, forStoreKey storeKey: UserDefaultsStoreKey<Int64>) {
         userDefaults.setObject(value.map(NSNumber.init), forKey: storeKey.key)
         userDefaults.synchronize()
     }
     
-    func setUserDefaultsValue(value: Double, forKey storeKey: UserDefaultsStoreKey<Double>) {
+    func setNewValue(value: Double, forStoreKey storeKey: UserDefaultsStoreKey<Double>) {
         userDefaults.setObject(value, forKey: storeKey.key)
         userDefaults.synchronize()
     }
     
-    func setUserDefaultsValue(value: String, forKey storeKey: UserDefaultsStoreKey<String>) {
+    func setNewValue(value: String, forStoreKey storeKey: UserDefaultsStoreKey<String>) {
         userDefaults.setObject(value, forKey: storeKey.key)
         userDefaults.synchronize()
     }
