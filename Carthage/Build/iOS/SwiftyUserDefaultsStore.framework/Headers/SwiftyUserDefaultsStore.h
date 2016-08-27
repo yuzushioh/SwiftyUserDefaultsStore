831097740887f4d640f026ac147813c5e00f303c
Ryo <eeee.jmk@gmail.com>
2016-08-27 20:08:19 +0900

    gitignoreを削除

---
+++ Carthage/Build/iOS/SwiftyUserDefaultsStore.framework/Headers/SwiftyUserDefaultsStore.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyUserDefaultsStore.h
+//  SwiftyUserDefaultsStore
+//
+//  Created by 福田涼介 on 8/27/16.
+//  Copyright © 2016 yuzushioh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyUserDefaultsStore.
+FOUNDATION_EXPORT double SwiftyUserDefaultsStoreVersionNumber;
+
+//! Project version string for SwiftyUserDefaultsStore.
+FOUNDATION_EXPORT const unsigned char SwiftyUserDefaultsStoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyUserDefaultsStore/PublicHeader.h>
+
+
