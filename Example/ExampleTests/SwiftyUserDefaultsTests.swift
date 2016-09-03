51ad26e1ebef39850ae5212c1978f20eaa40a373
Ryo <eeee.jmk@gmail.com>
2016-09-03 22:01:58 +0900

    Added Tests

---
+++ Example/ExampleTests/SwiftyUserDefaultsTests.swift
@@ -0,0 +1,41 @@
+//
+//  SwiftyUserDefaultsTests.swift
+//  Example
+//
+//  Created by 福田涼介 on 9/3/16.
+//  Copyright © 2016 yuzushioh. All rights reserved.
+//
+
+import XCTest
+@testable import Example
+
+class SwiftyUserDefaultsTests: XCTestCase {
+    
+    func testInteger() {
+        let integer = StoreKey<Int>(key: "integer")
+        let value = 100
+        integer.value = value
+        XCTAssertEqual(integer.value, 100)
+    }
+    
+    func testString() {
+        let string = StoreKey<String>(key: "string")
+        let value = "yuzushioh"
+        string.value = value
+        XCTAssertEqual(string.value, value)
+    }
+    
+    func testBool() {
+        let bool = StoreKey<Bool>(key: "bool")
+        let value = false
+        bool.value = value
+        XCTAssertEqual(bool.value, value)
+    }
+    
+    func testDouble() {
+        let double = StoreKey<Double>(key: "double")
+        let value = 10.5
+        double.value = value
+        XCTAssertEqual(double.value, value)
+    }
+}
